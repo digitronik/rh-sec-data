a19ebb7b1a3fa77f13ec1fd843db3fb1a83d0d4e
Nikhil Dhandre <nik.digitronik@live.com>
2021-03-19 20:08:31 +0530

    Adding cli entrypoint

---
+++ src/rh_sec_data/__init__.py
@@ -0,0 +1,4 @@
+from rh_sec_data.api import get_cve
+from rh_sec_data.api import get_cvrf
+from rh_sec_data.api import get_ovel
+from rh_sec_data.api import get_ovelstream
---
+++ src/rh_sec_data/cli.py
@@ -0,0 +1,6 @@
+import click
+
+@click.group()
+def main():
+    """Red Hat Security Data API."""
+    pass
